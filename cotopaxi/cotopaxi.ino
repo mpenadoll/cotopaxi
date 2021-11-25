6f0d8b0d0c16e0491f627ef26c61b514a6689982
Mateo Pena Doll <mpenadoll@gmail.com>
2021-11-24 16:24:55 -0800

    good morning mary!

--- cotopaxi/cotopaxi.ino
+++ cotopaxi/cotopaxi.ino
@@ -44,7 +44,7 @@ void setup()
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println(F("Hey, Hot Stuff!"));
+  display.println(F("Good Morning, Mary!"));
   display.display();
 
   delay(3000);
